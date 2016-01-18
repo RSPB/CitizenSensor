1c50de60ffa09eeeb0e82017bfd1bad43c64b1b9
tracek <lukasz.tracewski@gmail.com>
2016-01-18 13:33:55 +0100

    Make location reversal optional

--- gps.py
+++ gps.py
@@ -1,64 +1,64 @@
 #!/usr/bin/env python
 
-"""
-Created on Sat Nov 28 12:51:07 2015
-
-@author: tracek
-"""
-
 from __future__ import division
 import exifread
 from datetime import datetime
 from geopy.geocoders import Nominatim
 
-class GPSLocator(object):
 
-    def __init__(self):
-        self.geolocator = Nominatim()
+geolocator = Nominatim()
 
-    def _convert_to_deg(self, value):
-        degrees = value[0].num / value[0].den
-        minutes = value[1].num / value[1].den
-        seconds = value[2].num / value[2].den
-        return degrees + minutes / 60.0 + seconds / 3600.0
+def _convert_to_deg(self, value):
+    degrees = value[0].num / value[0].den
+    minutes = value[1].num / value[1].den
+    seconds = value[2].num / value[2].den
+    return degrees + minutes / 60.0 + seconds / 3600.0
 
-    def get_gps_metadata(self, filepath):
-        result = {}
 
+def get_gps_metadata(filepath, reverse_location=False):
+    result = {}
+
+    try:
+        f = open(filepath, 'rb')
+        tags = exifread.process_file(f)
+        GPSDate = tags['GPS GPSDate'].values
+        date = datetime.strptime(GPSDate, '%Y:%m:%d')
+
+        GPSAltitude = tags['GPS GPSAltitude'].values
+        altitude = eval(str(GPSAltitude[0]))
+
+        GPSLatitude = tags['GPS GPSLatitude'].values
+        GPSLatitudeRef = tags['GPS GPSLatitudeRef'].values
+        latitude = _convert_to_deg(GPSLatitude) if GPSLatitudeRef == 'N' else -_convert_to_deg(GPSLatitude)
+
+        GPSLongitude = tags['GPS GPSLongitude'].values
+        GPSLongitudeRef = tags['GPS GPSLongitudeRef'].values
+        longitude = _convert_to_deg(GPSLongitude) if GPSLongitudeRef == 'E' else -_convert_to_deg(GPSLongitude)
+
+        GPSMapDatum = tags['GPS GPSMapDatum'].values
+
+        result['coordinates'] = (latitude, longitude)
+        result['altitude'] = altitude
+        result['location'] = geolocator.reverse((latitude, longitude)).address if reverse_location else ''
+        result['datum'] = GPSMapDatum
+        result['date'] = str(date.date())
+    except:
+        result['coordinates'] = 'Unknown'
+        result['altitude'] = 'Unknown'
+        result['location'] = 'Unknown'
+        result['datum'] = 'Unknown'
         try:
-            f = open(filepath, 'rb')
-            tags = exifread.process_file(f)
-            GPSDate = tags['GPS GPSDate'].values
-            date = datetime.strptime(GPSDate, '%Y:%m:%d')
-
-            GPSAltitude = tags['GPS GPSAltitude'].values
-            altitude = eval(str(GPSAltitude[0]))
-
-            GPSLatitude = tags['GPS GPSLatitude'].values
-            GPSLatitudeRef = tags['GPS GPSLatitudeRef'].values
-            latitude = self._convert_to_deg(GPSLatitude) if GPSLatitudeRef == 'N' else -self._convert_to_deg(GPSLatitude)
-
-            GPSLongitude = tags['GPS GPSLongitude'].values
-            GPSLongitudeRef = tags['GPS GPSLongitudeRef'].values
-            longitude = self._convert_to_deg(GPSLongitude) if GPSLongitudeRef == 'E' else -self._convert_to_deg(GPSLongitude)
-
-            GPSMapDatum = tags['GPS GPSMapDatum'].values
-
-            result['coordinates'] = (latitude, longitude)
-            result['altitude'] = altitude
-            result['location'] = self.geolocator.reverse((latitude, longitude)).address
-            result['datum'] = GPSMapDatum
-            result['date'] = str(date.date())
-        except:
-            result['coordinates'] = 'Unknown'
-            result['altitude'] = 'Unknown'
-            result['location'] = 'Unknown'
-            result['datum'] = 'Unknown'
             result['date'] = tags['EXIF DateTimeOriginal'].values if 'EXIF DateTimeOriginal' in tags else 'Unknown'
+        except:
+            result['date'] = 'Unknown'
 
-        return result
+    return result
 
 if __name__ == '__main__':
-    locator = GPSLocator()
-    metadata = locator.get_gps_metadata('/home/tracek/Notebooks/data/27302080E.jpg')
+    import argparse
+    import configure
+
+    parser = argparse.ArgumentParser(description='GPS data parser', prog='Citizen Sensor')
+    parser.add_argument('-i', '--image', help='Path to an image', type=configure.check_file_exist, required=True)
+    metadata = get_gps_metadata('/home/tracek/Notebooks/data/27302080E.jpg')
     print metadata
