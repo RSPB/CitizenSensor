d3b2fe031bd92b5ebafce17613150b4d96c48b2c
tracek <lukasz.tracewski@gmail.com>
2016-08-24 16:47:38 +0200

    Cleanup

--- url_load.py
+++
@@ -1,9 +0,0 @@
-import urllib
-import os
-from urlparse import urlparse
-
-testfile = urllib.URLopener()
-path = "https://farm1.static.flickr.com/744/21456282281_581c0dba50.jpg"
-parsed_address = urlparse(path)
-
-testfile.retrieve("https://farm1.static.flickr.com/744/21456282281_581c0dba50.jpg", "21456282281_581c0dba50.jpg")
